9ef646ff56994664b83d03c610468a5361935311
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-01 00:07:30 +0300

    Add google map

--- resources/views/frontend/pages_frontend/contact/create.blade.php
+++ resources/views/frontend/pages_frontend/contact/create.blade.php
@@ -87,8 +87,10 @@
 
 
 			<!-- Google Map _______________________ -->
-			<div id="google-map-area">
+			{{-- <div id="google-map-area">
 				<div class="google-map" id="contact-google-map" data-map-lat="0.313903" data-map-lng="32.579711" data-map-title="Find Map" data-map-zoom="12"></div>
-	   		 </div>
+	   		 </div> --}}
+
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3989.7516120917426!2d32.576434450157954!3d0.33132289975709134!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x177dbb74c66550b5%3A0x5bf468a846e19c64!2sAHA%20Towers!5e0!3m2!1sen!2sug!4v1672520793433!5m2!1sen!2sug" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 @endsection
