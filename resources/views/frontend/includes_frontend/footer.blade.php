3a6cd3e3ac3b653a6e4712b33c9a8b3d93af4248
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-31 14:01:00 +0300

    Add footer include

--- resources/views/frontend/includes_frontend/footer.blade.php
+++ resources/views/frontend/includes_frontend/footer.blade.php
@@ -1,99 +1,81 @@
-<footer id="rs-footer" class="rs-footer">
-            <div class="footer-top">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget md-mb-50">
-                            <h4 class="widget-title">Quick Links</h4>
-                            <ul class="site-map">
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Courses</a></li>
-                                <li><a href="#">About Us</a></li>
-                                <li><a href="contacts.html">Contact Us</a></li>
-                                <li><a href="#">Gallery</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget md-mb-50">
-                            <h4 class="widget-title">School</h4>
-                            <ul class="site-map">
-                                <li><a href="#">About</a></li>
-                                <li><a href="staff.html">Staff</a></li>
-                                <li><a href="faq.html">FAQ's</a></li>
-                                <li><a href="#">Testimonials</a></li>
-                                <li><a href="volunteer.html">Volunteer</a></li>
-                            </ul>
-                        </div>
-                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget md-mb-50">
-                            <h4 class="widget-title">Courses</h4>
-                            <ul class="site-map">
-                                <li><a href="#">Dip. Clinical Medicine</a></li>
-                                <li><a href="#">Dip. Dentistry</a></li>
-                                <li><a href="#">Nursing & Midwifery</a></li>
-                                <li><a href="#">Dip. Medical Lab. Technology</a></li>
-                                <li><a href="#">Dip. Public Health></li>
-                            </ul>
-                        </div>
-                        
-                        <div class="col-lg-3 col-md-12 col-sm-12 footer-widget">
-                            <h4 class="widget-title">Address</h4>
-                            <ul class="address-widget">
-                                <li>
-                                    <i class="flaticon-location"></i>
-                                    <div class="desc">Maya - Nakirebe along Kampala - Masaka Road,</div>
-                                </li>
-                                <!-- <li>
-                                    <i class="flaticon-email">
-                                        <div class="desc">
-                                            P.O BOX 72128, Kampala, Uganda
-                                        </div> 
-                                    </i>
-                                </li> -->
-                                <li>
-                                    <i class="flaticon-call"></i>
-                                    <div class="desc">
-                                    <ul>
-                                        <a href="tel:(123)-456-7890">+256-392 00 1503 </a> ,<br>
-                                        <a href="tel:(123)-456-7890">+256-704789181</a> ,<br>
-                                        <a href="tel:(123)-456-7890">+256-775 534 797 (whatsapp)</a>
-                                    </ul>
-                                    </div>
-                                </li>
-                                <li>
-                                    <i class="flaticon-email"></i>
-                                    <div class="desc">
-                                        <a href="mailto:infoname@gmail.com">info.ipim@gmail.com</a> , 
-                                        <a href="http://paramedicalinstitute.org">http://paramedicalinstitute.org</a>
-                                    </div>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-bottom">
-                <div class="container">                    
-                    <div class="row y-middle">
-                        <div class="col-lg-4 md-mb-20">
-                            <div class="footer-logo md-text-center">
-                                <a href="index2.html"><img src="{{ asset('assets/frontend/images/pre-logo.png') }}" alt="" style="size: 0.5cm;"></a>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 md-mb-20">
-                            <div class="copyright text-center md-text-left">
-                                <!-- <p>&copy; 2022 All Rights Reserved. Developed By <a href="http://rstheme.com/">RSTheme</a></p> -->
-                                
-                                <p>&copy; 2022 All Rights Reserved.</p>
-                            </div>
-                        </div>
-                        <div class="col-lg-4 text-right md-text-left">
-                            <ul class="footer-social">
-                                <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                <li><a href="#"><i class="fa fa-twitter"></i></a></li>
-                                <li><a href="#"><i class="fa fa-instagram"></i></a></li>
-                                <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
-                                <li><a href="#"><i class="fa fa-pinterest-p"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </footer>
+<footer class="bg-one">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-3 col-sm-6 col-xs-12">
+							<div class="footer-logo">
+								<a href="#"><img src="assets/frontend/images/logo/logo02.png" alt="Logo"></a>
+								<!-- <h5><a href="#" class="tran3s">consulting@gold-serve.com</a></h5>
+								<h6 class="p-color">202. 277. 3894</h6> -->
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						
+						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
+							<h4>Uganda</h4>
+							<ul>
+								<li><a href="#" class="tran3s">4th Floor Aha Towers</a></li>
+								<li><a href="#" class="tran3s">Lourdel Road,</a></li>
+								<li><a href="#" class="tran3s">Telephone: +256-312-531-682</a></li>
+								<li><a href="#" class="tran3s">consulting@gold-serve.com</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
+							<h4>Tanzania</h4>
+							<ul>
+								<li><a href="#" class="tran3s">9th Floor Faykat Tower</a></li>
+								<li><a href="#" class="tran3s">Ali Hassan Mwinyi Road,</a></li>
+								<li><a href="#" class="tran3s">Dar es Salam, Tanzania</a></li>
+								<li><a href="#" class="tran3s">tz.office@gold-serve.com</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
+							<h4>Our Services</h4>
+							<ul>
+								<li><a href="#" class="tran3s">Business Support Services</a></li>
+								<li><a href="training.html" class="tran3s">Trainings</a></li>
+								<li><a href="techncal_mpower.html" class="tran3s">Technical Manpower Services</a></li>
+								<li><a href="#" class="tran3s">General Supplies and Works</a></li>
+							</ul>
+						</div>
+						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
+							<h4>Legal</h4>
+							<p>2020 GoldServe is the trading name of Goldleaf Holdings Ltd, a limited liability company registered in the Republic of Uganda under
+								Registration: No. 71759,
+								VAT No. 1012737313</p>
+							<!-- <ul>
+								<li><a href="#" class="tran3s">About Singleton</a></li>
+								<li><a href="#" class="tran3s">Jobs</a></li>
+								<li><a href="#" class="tran3s">Team</a></li>
+								<li><a href="#" class="tran3s">Testimonials</a></li>
+								<li><a href="#" class="tran3s">Blog</a></li>
+							</ul> -->
+						</div>
+						<!-- <div class="col-md-3 col-sm-6 col-xs-12 footer-subscribe">
+							<h4>Subscribe Us</h4>
+							<form action="#">
+								<input type="text" placeholder="Enter your mail">
+							</form>
+							<ul>
+								<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+							</ul>
+						</div> -->
+					</div> <!-- /.row -->
+
+					<div class="bottom-footer clearfix">
+						<ul class="float-right">
+							<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="https://twitter.com/GoldServe_QHSE" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="https://www.linkedin.com/company/goldserve" class="tran3s"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+							<!-- <li><h3><span class="timer p-color" data-from="0" data-to="8997" data-speed="5000" data-refresh-interval="50">0</span> Products</h3></li>
+							<li><h3><span class="timer p-color" data-from="0" data-to="53701" data-speed="5000" data-refresh-interval="50">0</span> Members</h3></li>
+							<li><h3><span class="timer p-color" data-from="0" data-to="1110" data-speed="5000" data-refresh-interval="50">0</span> Shops</h3></li> -->
+						</ul>
+						<p class="float-left">&copy; 2023 <a href="#" class="tran3s p-color">GoldServe</a>. All rights reserved</p>
+					</div>
+				</div> <!-- /.container -->
+			</footer>
--- resources/views/frontend/layouts_frontend/master.blade.php
+++ resources/views/frontend/layouts_frontend/master.blade.php
@@ -514,87 +514,12 @@
 				Footer
 			============================================== 
 			-->
-			<footer class="bg-one">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-3 col-sm-6 col-xs-12">
-							<div class="footer-logo">
-								<a href="#"><img src="assets/frontend/images/logo/logo02.png" alt="Logo"></a>
-								<!-- <h5><a href="#" class="tran3s">consulting@gold-serve.com</a></h5>
-								<h6 class="p-color">202. 277. 3894</h6> -->
-							</div>
-						</div>
-					</div>
-					<div class="row">
-						
-						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
-							<h4>Uganda</h4>
-							<ul>
-								<li><a href="#" class="tran3s">4th Floor Aha Towers</a></li>
-								<li><a href="#" class="tran3s">Lourdel Road,</a></li>
-								<li><a href="#" class="tran3s">Telephone: +256-312-531-682</a></li>
-								<li><a href="#" class="tran3s">consulting@gold-serve.com</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
-							<h4>Tanzania</h4>
-							<ul>
-								<li><a href="#" class="tran3s">9th Floor Faykat Tower</a></li>
-								<li><a href="#" class="tran3s">Ali Hassan Mwinyi Road,</a></li>
-								<li><a href="#" class="tran3s">Dar es Salam, Tanzania</a></li>
-								<li><a href="#" class="tran3s">tz.office@gold-serve.com</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
-							<h4>Our Services</h4>
-							<ul>
-								<li><a href="#" class="tran3s">Business Support Services</a></li>
-								<li><a href="training.html" class="tran3s">Trainings</a></li>
-								<li><a href="techncal_mpower.html" class="tran3s">Technical Manpower Services</a></li>
-								<li><a href="#" class="tran3s">General Supplies and Works</a></li>
-							</ul>
-						</div>
-						<div class="col-md-3 col-sm-6 col-xs-12 footer-list">
-							<h4>Legal</h4>
-							<p>2020 GoldServe is the trading name of Goldleaf Holdings Ltd, a limited liability company registered in the Republic of Uganda under
-								Registration: No. 71759,
-								VAT No. 1012737313</p>
-							<!-- <ul>
-								<li><a href="#" class="tran3s">About Singleton</a></li>
-								<li><a href="#" class="tran3s">Jobs</a></li>
-								<li><a href="#" class="tran3s">Team</a></li>
-								<li><a href="#" class="tran3s">Testimonials</a></li>
-								<li><a href="#" class="tran3s">Blog</a></li>
-							</ul> -->
-						</div>
-						<!-- <div class="col-md-3 col-sm-6 col-xs-12 footer-subscribe">
-							<h4>Subscribe Us</h4>
-							<form action="#">
-								<input type="text" placeholder="Enter your mail">
-							</form>
-							<ul>
-								<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-								<li><a href="#" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-								<li><a href="#" class="tran3s"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
-								<li><a href="#" class="tran3s"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-							</ul>
-						</div> -->
-					</div> <!-- /.row -->
+			
 
-					<div class="bottom-footer clearfix">
-						<ul class="float-right">
-							<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
-								<li><a href="https://twitter.com/GoldServe_QHSE" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
-								<li><a href="https://www.linkedin.com/company/goldserve" class="tran3s"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
-								<li><a href="#" class="tran3s"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
-							<!-- <li><h3><span class="timer p-color" data-from="0" data-to="8997" data-speed="5000" data-refresh-interval="50">0</span> Products</h3></li>
-							<li><h3><span class="timer p-color" data-from="0" data-to="53701" data-speed="5000" data-refresh-interval="50">0</span> Members</h3></li>
-							<li><h3><span class="timer p-color" data-from="0" data-to="1110" data-speed="5000" data-refresh-interval="50">0</span> Shops</h3></li> -->
-						</ul>
-						<p class="float-left">&copy; 2023 <a href="#" class="tran3s p-color">GoldServe</a>. All rights reserved</p>
-					</div>
-				</div> <!-- /.container -->
-			</footer>
+             <!-- Footer Start -->
+                @include('frontend.includes_frontend.footer')
+            <!-- Footer End -->
+
 
 	        
 
