795719f9482e82d6c6f21993b939092034d315f7
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-31 22:45:16 +0300

    Modify single page

--- resources/views/frontend/pages_frontend/articles/show.blade.php
+++ resources/views/frontend/pages_frontend/articles/show.blade.php
@@ -5,87 +5,114 @@
 @endsection
 
 @section('content')
- <div class="main-content">
-           <div class="rs-breadcrumbs breadcrumbs-overlay">
-					<div class="breadcrumbs-img">
-							<img src="{{ asset('assets/frontend/images/breadcrumbs/9.png') }}" alt="Breadcrumbs Image">
-					</div>
-					<div class="breadcrumbs-text white-color">
-							<h1 class="page-title">Post Details</h1>
-							<ul>
-								<li>
-									<a class="active" href="index-2.html">Home</a>
-								</li>
-								<li>Post Details</li>
+
+
+
+			<div class="inner-page-banner">
+				<div class="opacity">
+					<h1>Blog Details</h1>
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li>/</li>
+						<li>Blog Details</li>
+					</ul>
+				</div> <!-- /.opacity -->
+			</div> <!-- /inner-page-banner -->
+
+
+
+			<!--
+			=============================================
+				Blog Details
+			==============================================
+			-->
+			<div class="blog-details blog-v3">
+				<div class="container">
+					<div class="wrapper">
+						<div class="blog-main-post">
+                            @if (($post->post_photo))
+                            <div class="single-blog">
+                                <div class="image">
+                                    <img src="{{ asset($post->post_photo) }}" alt="">
+                                </div>
+                                @else
+                                <p class="text-danger">No Photo</p>
+                            @endif
+
+							<h6>{{ $post->post_title }}</h6>
+                            <h6 style="margin-top: 20px;">By: {{ $post->post_created_by }}</h6>
+							<p>{{ $post->post_description }} </p> <br>
+
+						</div> <!-- /.blog-main-post -->
+						{{-- <div class="tag-option clearfix">
+							<ul class="float-left">
+								<li>Tag:</li>
+								<li><a href="#" class="tran3s">Travel,</a></li>
+								<li><a href="#" class="tran3s">Advanture,</a></li>
+								<li><a href="#" class="tran3s">Landscape</a></li>
 							</ul>
-					</div>
-			</div>
-			<!-- Breadcrumbs End -->
-            
-           <!-- Blog Section Start -->
-            <div class="rs-inner-blog orange-color pt-100 pb-100 md-pt-70 md-pb-70">
-                <div class="container">
-                   <div class="blog-deatails">
-                        <div class="bs-img">
-                            <a href="#"><img src="{{ asset($post->post_photo) }}" alt=""></a>
-                        </div>
-                       <div class="blog-full">
-                           <ul class="single-post-meta">
-                               <li>
-                                   <span class="p-date"> <i class="fa fa-calendar-check-o"></i> {{ $post->created_at }} </span>
-                               </li> 
-                               <li>
-                                   <span class="p-date"> <i class="fa fa-user-o"></i> {{ $post->post_created_by }}  </span>
-                               </li> 
-                               <li class="Post-cate">
-                                   <div class="tag-line">
-                                       <i class="fa fa-book"></i>
-                                       <a href="#">{{ $post->post_category_r->post_category_name }}</a>
-                                   </div>
-                               </li>
-                           </ul>
-                           <h2>{{ $post->post_title }}</h2>
-                           <div class="blog-desc">
-                               <p>
-                                {{ $post->post_description}}    
-                                 </p>
-                           </div>
-
-                           <!-- discuss comments -->
-
-                           <div id="disqus_thread"></div>
-                                        <script>
-                                            /**
-                                            *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-                                            *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables    */
-                                          
-                                            // var disqus_config = function () {
-                                            // this.page.url = {{ Request::url()}};  // Replace PAGE_URL with your page's canonical URL variable
-                                            // this.page.identifier = {{ $post->id}}; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-                                            // };
-                                           
-                                            (function() { // DON'T EDIT BELOW THIS LINE
-                                            var d = document, s = d.createElement('script');
-                                            s.src = 'https://website-yk9vnplwgy.disqus.com/embed.js';
-                                            s.setAttribute('data-timestamp', +new Date());
-                                            (d.head || d.body).appendChild(s);
-                                            })();
-                            </script>
-                            <noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+							<ul class="float-right">
+								<li>Share:</li>
+								<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+							</ul>
+						</div> --}}
 
 
+					</div> <!-- /.wrapper -->
+				</div> <!-- /.container -->
+
+				<div class="comment-area">
+					<div class="container">
+						<div class="wrapper">
+							<div class="comment-section">
+								<h2>2 comments</h2>
+								<div class="single-comment clearfix">
+									<img src="images/blog/20.jpg" alt="" class="float-left">
+									<div class="comment float-left">
+										<h6>Jannatul Fa.</h6>
+										<span>15 April, 2016 At 8:30am</span>
+										<p>One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in future. The same true we experience the emotional sensation of stress from our first.</p>
+										<button class="tran3s">Reply</button>
+									</div>
+								</div> <!-- /.single-comment -->
+								<div class="single-comment clearfix no-border">
+									<img src="images/blog/21.jpg" alt="" class="float-left">
+									<div class="comment float-left">
+										<h6>Jannatul Fa.</h6>
+										<span>15 April, 2016 At 8:30am</span>
+										<p>One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in future</p>
+										<button class="tran3s">Reply</button>
+									</div>
+								</div> <!-- /.single-comment -->
+							</div> <!-- /.comment-section -->
+
+							<div class="leave-comment">
+								<h2>Leave A Comment</h2>
+								<p>sing in to post your comment or singup if you dont have any account.</p>
+								<form action="#">
+									<div class="row">
+										<div class="col-sm-6">
+											<input type="text" placeholder="Name *">
+										</div>
+										<div class="col-sm-6">
+											<input type="email" placeholder="Email *">
+										</div>
+										<div class="col-xs-12">
+											<textarea placeholder="Your Comment"></textarea>
+										</div>
+									</div>
+									<button class="p-bg-color hvr-trim-two">Post COmment</button>
+								</form>
+							</div> <!-- /.leave-comment -->
+						</div> <!-- /.wrapper -->
+					</div> <!-- /.container -->
+				</div> <!-- /.comment-area -->
+			</div> <!-- /.blog-details -->
 
 
-                           <!-- end of disquss comments -->
-                         
-                         
-                       </div>
-                   </div>
-                   
-                 
-                </div>
-            </div>
-            <!-- Blog Section End -->  
-            
-        </div> 
+
+
 @endsection
