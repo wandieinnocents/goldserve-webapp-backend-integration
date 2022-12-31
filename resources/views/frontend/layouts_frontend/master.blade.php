fa5a134bfc6421e7c8ac14a8643860f135129a1b
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-31 13:56:43 +0300

    Add header layout

--- resources/views/frontend/layouts_frontend/master.blade.php
+++ resources/views/frontend/layouts_frontend/master.blade.php
@@ -15,7 +15,7 @@
 		<!-- iOS Safari -->
 		<meta name="apple-mobile-web-app-status-bar-style" content="#2c2c2c">
 
-		<title>Goldserve - Uganda | Consulting Training | Safety Supplies</title>
+		<title>@yield('title')</title>
 
 		<!-- Favicon -->
 		<link rel="icon" type="image/png" sizes="56x56" href="assets/frontend/images/fav-icon/icon.png">
@@ -64,74 +64,7 @@
 				Theme Header
 			============================================== 
 			-->
-			<header class="theme-menu-wrapper menu-style-two">
-				<div class="header-wrapper">
-					<div class="container">
-						<!-- Logo -->
-						<div class="logo float-left tran4s"><a href="/"><img src="assets/frontend/images/logo/logo02.png" alt="Logo"></a></div>
-
-						<!-- ============================ Theme Menu ========================= -->
-						<nav class="theme-main-menu float-right navbar" id="mega-menu-wrapper">
-							<!-- Brand and toggle get grouped for better mobile display -->
-						   <div class="navbar-header">
-						     <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1" aria-expanded="false">
-						       <span class="sr-only">Toggle navigation</span>
-						       <span class="icon-bar"></span>
-						       <span class="icon-bar"></span>
-						       <span class="icon-bar"></span>
-						     </button>
-						   </div>
-						   <!-- Collect the nav links, forms, and other content for toggling -->
-						   <div class="collapse navbar-collapse" id="navbar-collapse-1">
-								<ul class="nav">
-									<li class="dropdown-holder menu-list active"><a href="index-2.html" class="tran3s">Home</a>
-										<!-- <ul class="sub-menu">
-											<li><a href="index.html">Home version one</a></li>
-											<li><a href="index-2.html">Home version Two</a></li>
-											<li><a href="index-3.html">Home version Three</a></li>
-											<li><a href="index-video.html">Home video version</a></li>
-										</ul> -->
-									</li>
-									<li><a href="about-us.html">About Us</a></li>
-									<li class="dropdown-holder menu-list"><a href="#" class="tran3s">Services</a>
-										<ul class="sub-menu">
-											
-											<li class="dropdown-holder"><a href="blog-v3.html">Business Support Services</a>
-												<ul class="second-sub-menu">
-													<li><a href="facility_management.html">Facility Management</a></li>
-													<li><a href="expat_orientaion.html">Expat Services</a></li>
-													<li><a href="">Vendor Management</a></li>
-													<li><a href="consulting_services.html">Consulting Services Management</a></li>
-													<li><a href="qshe_services.html">QSHE Services</a></li>
-												</ul>
-											</li>
-											<li><a href="techncal_mpower.html">Techninal ManPower Services</a></li>
-											<li class="dropdown-holder"><a href="shop.html">General Supplies and Works</a>
-												<ul class="second-sub-menu">
-													<li><a href="niche_supplies.html">Niche Supplies</a></li>
-													<li><a href="procurement.html">Procurement Buy House </a></li>
-													<li><a href="works.html">Works</a></li>
-												</ul>
-											</li>
-											<!-- <li><a href="about-us.html">About us</a></li>
-											<li><a href="team-v1.html">Team Version one</a></li>
-											<li><a href="team-v2.html">Team Version Two</a></li> -->
-										</ul>
-									</li>
-									
-								    <li><a href="service-details.html">Testimonials</a></li>
-									<li><a href="blogs.html">Blogs</a></li>
-										
-									<li><a href="training.html">Trainings</a></li>
-										
-									<li class="menu-list"><a href="contact.html" class="tran3s">Contact Us</a></li>
-									<!-- <li class="login-button"><a href="#" class="tran3s" data-toggle="modal" data-target=".signUpModal">login</a></li> -->
-								</ul>
-						   </div><!-- /.navbar-collapse -->
-						</nav> <!-- /.theme-main-menu -->
-					</div> <!-- /.clearfix -->
-				</div>
-			</header> <!-- /.theme-menu-wrapper -->
+			@include('frontend.includes_frontend.header_frontend')
 			
 			<!-- 
 			=============================================
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('frontend.layouts_frontend.master')
 
 @section('title')
-
+Goldserve - Uganda | Consulting Training | Safety Supplies
 @endsection
 
 @section('content')
