e6ca081d0c21056cc14c91e684117e1e7622c2dd
wandieinnocents <wandieinnocent2@gmail.com>
2022-12-31 21:22:46 +0300

    Modify css , links

--- resources/views/frontend/layouts_frontend/master.blade.php
+++ resources/views/frontend/layouts_frontend/master.blade.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-	
+
 <head>
 		<meta charset="UTF-8">
 		<!-- For IE -->
@@ -18,13 +18,13 @@
 		<title>@yield('title')</title>
 
 		<!-- Favicon -->
-		<link rel="icon" type="image/png" sizes="56x56" href="assets/frontend/images/fav-icon/icon.png">
+		<link rel="icon" type="image/png" sizes="56x56" href="{{ asset('assets/frontend/images/fav-icon/icon.png') }}">
 
 
 		<!-- Main style sheet -->
-		<link rel="stylesheet" type="text/css" href="assets/frontend/css/style.css">
+		<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/style.css') }}">
 		<!-- responsive style sheet -->
-		<link rel="stylesheet" type="text/css" href="assets/frontend/css/responsive.css">
+		<link rel="stylesheet" type="text/css" href="{{ asset('assets/frontend/css/responsive.css') }}">
 
 
 		<!-- Fix Internet Explorer ______________________________________-->
@@ -35,7 +35,7 @@
 			<script src="vendor/respond.js"></script>
 		<![endif]-->
 
-			
+
 	</head>
 
 	<body>
@@ -59,29 +59,29 @@
 
 
 
-			<!-- 
+			<!--
 			=============================================
 				Theme Header
-			============================================== 
+			==============================================
 			-->
 			@include('frontend.includes_frontend.header_frontend')
-			
-			<!-- 
+
+			<!--
 			=============================================
 				Theme Main Banner
-			============================================== 
+			==============================================
 			-->
-			
-           
+
+
 
             @yield('content')
-            
+
              <!-- Footer Start -->
                 @include('frontend.includes_frontend.footer')
             <!-- Footer End -->
 
 
-	        
+
 
 	        <!-- Scroll Top Button -->
 			<button class="scroll-top tran3s">
@@ -127,33 +127,33 @@
 		<!-- Js File_________________________________ -->
 
 		<!-- j Query -->
-		<script type="text/javascript" src="assets/frontend/vendor/jquery.2.2.3.min.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/jquery.2.2.3.min.js') }}"></script>
 		<!-- Bootstrap Select JS -->
-		<script type="text/javascript" src="assets/frontend/vendor/bootstrap-select/dist/js/bootstrap-select.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/bootstrap-select/dist/js/bootstrap-select.js') }}"></script>
 
 		<!-- Bootstrap JS -->
-		<script type="text/javascript" src="assets/frontend/vendor/bootstrap/bootstrap.min.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/bootstrap/bootstrap.min.js') }}"></script>
 
 		<!-- Vendor js _________ -->
 		<!-- Camera Slider -->
-		<script type='text/javascript' src='assets/frontend/vendor/Camera-master/scripts/jquery.mobile.customized.min.js'></script>
-	    <script type='text/javascript' src='assets/frontend/vendor/Camera-master/scripts/jquery.easing.1.3.js'></script> 
-	    <script type='text/javascript' src='assets/frontend/vendor/Camera-master/scripts/camera.min.js'></script>
+		<script type='text/javascript' src='{{ asset('assets/frontend/vendor/Camera-master/scripts/jquery.mobile.customized.min.js') }}'></script>
+	    <script type='text/javascript' src='{{ asset('assets/frontend/vendor/Camera-master/scripts/jquery.easing.1.3.js') }}'></script>
+	    <script type='text/javascript' src='{{ asset('assets/frontend/vendor/Camera-master/scripts/camera.min.js') }}'></script>
 	    <!-- Mega menu  -->
-		<script type="text/javascript" src="assets/frontend/vendor/bootstrap-mega-menu/js/menu.js"></script>
-		
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/bootstrap-mega-menu/js/menu.js') }}"></script>
+
 		<!-- WOW js -->
-		<script type="text/javascript" src="assets/frontend/vendor/WOW-master/dist/wow.min.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/WOW-master/dist/wow.min.js') }}"></script>
 		<!-- owl.carousel -->
-		<script type="text/javascript" src="assets/frontend/vendor/owl-carousel/owl.carousel.min.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/owl-carousel/owl.carousel.min.js') }}"></script>
 		<!-- js count to -->
-		<script type="text/javascript" src="assets/frontend/vendor/jquery.appear.js"></script>
-		<script type="text/javascript" src="assets/frontend/vendor/jquery.countTo.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/jquery.appear.js') }}"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/jquery.countTo.js') }}"></script>
 		<!-- Fancybox -->
-		<script type="text/javascript" src="assets/frontend/vendor/fancybox/dist/jquery.fancybox.min.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/vendor/fancybox/dist/jquery.fancybox.min.js') }}"></script>
 
 		<!-- Theme js -->
-		<script type="text/javascript" src="assets/frontend/js/theme.js"></script>
+		<script type="text/javascript" src="{{ asset('assets/frontend/js/theme.js') }}"></script>
 
 		</div> <!-- /.main-page-wrapper -->
 	</body>
--- resources/views/frontend/pages_frontend/contact/create.blade.php
+++ resources/views/frontend/pages_frontend/contact/create.blade.php
@@ -6,109 +6,84 @@
 
 @section('content')
 
- <!-- Breadcrumbs Start -->
-            <div class="rs-breadcrumbs breadcrumbs-overlay">
-                <div class="breadcrumbs-img">
-                    <img src="{{ asset('assets/frontend/images/breadcrumbs/6.jpg') }}" alt="Breadcrumbs Image">
-                </div>
-                <div class="breadcrumbs-text white-color padding">
-                    <h1 class="page-title">Contact </h1>
-                    <ul>
-                        <li>
-                            <a class="active" href="index2.html">Home</a>
-                        </li>
-                        <li>Contact </li>
-                    </ul>
-                </div>
-            </div>
-            <!-- Breadcrumbs End -->            
+	
+			<!-- 
+			=============================================
+				Theme Inner Banner 
+			============================================== 
+			-->
+			<div class="inner-page-banner">
+				<div class="opacity">
+					<h1>Contact US</h1>
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li>/</li>
+						<li>Contact</li>
+					</ul>
+				</div> <!-- /.opacity -->
+			</div> <!-- /inner-page-banner -->
 
-    		<!-- Contact Section Start -->
-    		<div class="contact-page-section pt-100 pb-100 md-pt-70 md-pb-70">
-            	<div class="container">
-            		<div class="row contact-address-section">
-        				<div class=" col-lg-4 col-md-12 lg-pl-0 md-mb-30">
-        					<div class="contact-info contact-address">
-        						<div class="icon-part">
-        							<i class="fa fa-map-marker" style="color: blue;"></i>
-        						</div>
-        						<div class="content-part">
-	        						<h5 class="info-subtitle">Address</h5>
-	        						<h4 class="info-title">Maya - Nakirebe along Kampala <br>Masaka Road </h4>
-	        					</div>
-        					</div>
-        				</div>
-        				<div class=" col-lg-4 col-md-12 md-mb-30">
-        					<div class="contact-info contact-mail">
-        						<div class="icon-part">
-        							<i class="fa fa-envelope-o" style="color: blue;"></i>
-        						</div>
-        						<div class="content-part">
-	        						<h5 class="info-subtitle">Email Address</h5>
-	        						<h4 class="info-title"><a href="mailto:info.ipim@gmail.com">info.ipim@gmail.com</a></h4>
-	        					</div>
-        					</div>
-        				</div>
-        				<div class=" col-lg-4 col-md-12 lg-pr-0">
-        					<div class="contact-info contact-phone">
-        						<div class="icon-part">
-        							<i class="fa fa-user-o" style="color: blue;"></i>
-        						</div>
-        						<div class="content-part">
-	        						<h5 class="info-subtitle">Phone Number</h5>
-	        						<h4 class="info-title"><a href="tel%2b0885898745.html">+256-392 00 1503  +256-704789181 +256-775 534 797</a></h4>
-	        					</div>
-        					</div>
-            			</div>
-            		</div>
-            		<div class="row">
-            			<div class="col-lg-5 md-mb-30">
-            				<div class="contact-map2">
-                               <iframe src="https://maps.google.com/maps?q=Maya%20%E2%80%93%20Nakirebe%20&t=&z=13&ie=UTF8&iwloc=&output=embed"></iframe>
-                            </div>
-            			</div>
-            			<div class="col-lg-7 pl-30 lg-pl-15">
-			        	    <div class="rs-quick-contact new-style">
-                                <div class="inner-part mb-50">
-                                    <h2 class="title mb-15">Get In Touch</h2>
-                                    <p>Get in touch with us using out ohone numbers, email , P.O Box or google map</p>
-                                </div>
-                                <div id="form-messages"></div>
-                                {{-- success message --}}
-                                 @if(session()->has('message'))
-                                    <div class="alert alert-info">
-                                        {{ session('message') }}
-                                    </div>
-                                @endif
-                                <form  method="post" action="{{ route('contact.store') }}">
-                                @csrf
-                                    <div class="row">
-                                        <div class="col-lg-6 mb-35 col-md-12">
-                                            <input class="from-control" type="text" id="name" name="name" placeholder="Name" required="">
-                                        </div> 
-                                        <div class="col-lg-6 mb-35 col-md-12">
-                                            <input class="from-control" type="text" id="email" name="email" placeholder="Email" required="">
-                                        </div>   
-                                        <div class="col-lg-6 mb-35 col-md-12">
-                                            <input class="from-control" type="text" id="phone" name="phone" placeholder="Phone" required="">
-                                        </div>   
-                                        <div class="col-lg-6 mb-35 col-md-12">
-                                            <input class="from-control" type="text" id="subject" name="subject" placeholder="Subject" required="">
-                                        </div>
-                                     
-                                        <div class="col-lg-12 mb-50">
-                                            <textarea class="from-control" id="message" name="description" placeholder=" Message" required=""></textarea>
-                                        </div>
-                                    </div>
-                                    <div class="form-group mb-0">
-                                        <input class="btn-send" style="background-color: blue;" type="submit" value="Submit Now">
-                                    </div>       
-                                </form>
-                            </div>
-                        </div> 
-            		</div>
-            	</div>
-            </div>
-            <!-- Contact Section End --> 
+
+
+			<!-- 
+			=============================================
+				Contact Us
+			============================================== 
+			-->
+			<div class="contact-us">
+				<div class="container">
+					<div class="row">
+						<div class="col-lg-7 col-md-6 col-xs-12">
+							<div class="contact-us-form">
+								<form action="https://html.creativegigstf.com/gullu/inc/sendemail.php" class="form-validation" autocomplete="off">
+									<input type="email" placeholder="Email Address*" name="email">
+									<input type="text" placeholder="Subject*" name="sub">
+									<textarea placeholder="Your Message*" name="message"></textarea>
+									<button class="p-bg-color hvr-trim-two">SEND MESSAGE</button>
+								</form>
+							</div> <!-- /.contact-us-form -->
+						</div> <!-- /.col- -->
+						<div class="col-lg-5 col-md-6 col-xs-12">
+							<div class="contact-address">
+								<h2>Don’t Hesitate to contact with us for any kind of information</h2>
+								<p>Call us for imiditate support this number</p>
+								<a href="#" class="tran3s">Uganda: +256-312-531-682</a>
+								<!-- <a href="#" class="tran3s">Tanzania: +256-312-531-682</a> -->
+								<ul>
+									<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+									<li><a href="https://twitter.com/GoldServe_QHSE" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+									<!-- <li><a href="#" class="tran3s"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li> -->
+									<li><a href="https://www.linkedin.com/company/goldserve" class="tran3s"><i class="fa fa-linkedin" aria-hidden="true"></i></a></li>
+								</ul>
+							</div> <!-- /.contact-address -->
+						</div> <!-- /.col- -->
+					</div> <!-- /.row -->
+				</div> <!-- /.conatiner -->
+
+				<!--Contact Form Validation Markup -->
+				<!-- Contact alert -->
+				<div class="alert-wrapper" id="alert-success">
+					<div id="success">
+						<button class="closeAlert"><i class="fa fa-times" aria-hidden="true"></i></button>
+						<div class="wrapper">
+			               	<p>Your message was sent successfully.</p>
+			             </div>
+			        </div>
+			    </div> <!-- End of .alert_wrapper -->
+			    <div class="alert-wrapper" id="alert-error">
+			        <div id="error">
+			           	<button class="closeAlert"><i class="fa fa-times" aria-hidden="true"></i></button>
+			           	<div class="wrapper">
+			               	<p>Sorry!Something Went Wrong.</p>
+			            </div>
+			        </div>
+			    </div> <!-- End of .alert_wrapper -->
+			</div> <!-- /.contact-us -->
+
+
+			<!-- Google Map _______________________ -->
+			<div id="google-map-area">
+				<div class="google-map" id="contact-google-map" data-map-lat="0.313903" data-map-lng="32.579711" data-map-title="Find Map" data-map-zoom="12"></div>
+	   		 </div>
     
 @endsection
