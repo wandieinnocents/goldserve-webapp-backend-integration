78278f9c5d4db433c03c88baa44b02f8d679b5b5
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-10 17:38:00 +0300

    Add manpower page

--- resources/views/frontend/pages_frontend/techncal_mpower/index.blade.php
+++ resources/views/frontend/pages_frontend/techncal_mpower/index.blade.php
@@ -1,7 +1,8 @@
 @extends('frontend.layouts_frontend.master')
 
 @section('title')
-Trainings
+Technical Manpower
+
 @endsection
 
 @section('content')
@@ -13,149 +14,111 @@ Trainings
 			-->
 			<div class="inner-page-banner">
 				<div class="opacity">
-					<h1>Trainings Services</h1>
+					<h1>Technical Manpower</h1>
 					<ul>
 						<li><a href="/">Home</a></li>
 						<li>/</li>
-						<li>Trainings</li>
+						<li>Technical Manpower</li>
 					</ul>
 				</div> <!-- /.opacity -->
 			</div> <!-- /inner-page-banner -->
 
 
-
-			<!--
+			<!-- 
 			=============================================
-				Our Service Details
-			==============================================
+				Blog Details
+			============================================== 
 			-->
-			<div class="service-details">
+			<div class="blog-details blog-v3">
 				<div class="container">
-					<div class="box-wrapper">
-						<img src="assets/frontend/images/home/bi2.png" alt="" class="feature-image">
-						<div class="title clearfix">
-							<h3 class="float-left">ANALYTICS COURSE</h3>
-							<a href="#" class="loan float-right">Apply Now</a>
-						</div> <!-- /.title -->
-						<div class="top-text">
-							<h4>This course is to impart hands on knowledge based on Power BI, a Business Intelligence and Data Analysis tool by Microsoft. The course will go through basics before introducing five components of Power BI (Power Pivot, Power Query, Power View, Power Map, and Power Q&A). At the end of the course you will be able to build dashboards based on different data sets and be ready to power your career with new edge knowledge in analytics that’s on demand across many sectors Analytics.</h4>
-                            <div class="row">
-								<div class="col-md-6 wow fadeInLeft"><p>
-                                    <ul>
+					<div class="wrapper">
+						<div class="blog-main-post">
+							<img src="assets/frontend/images/manpower.jpeg" alt="">
+							<h6>Best Qualified Persons</h6>
+							<!-- <h3>Challange yourself to win the future</h3> -->
+							<p>We manage the process of placing client-vetted, highly skilled technical manpower in-country and the associated welfare aspects related to their placement. Our on-demand professional employer organization (PEO) service, ensures that clients focus on their core operations as we handle the administrative tasks around contractor placement. Our on-demand service ensures that clients get critical aspects of their projects complete on time. Some of the areas that we have recently provided talent to are;
 
-                                        <li>
-                                            <h5>POWER BI ANALYTICS</h5>
-                                            <p>Power BI analytics course is to impart hands-on knowledge based on Power BI, a Business Intelligence and Data Analysis tool by Microsoft.</p></div>
-                                        </p>
-                                        </li>
-                                    </ul>
-                                    <!-- <div class="col-md-6 wow fadeInRight"><p>Sometimes this is airony promoting  businesse product and services, because for fact that you want make ur business recognizable & earn sales of course you need with lot of effort.</p></div> -->
-							</div>
-						</div> <!-- /.top-text -->
-
-						<div class="middle-text list-box-text wow fadeInUp">
-							<h3>FIVE COMPONENTS FOR POWER BI TRAINING COURSE</h3>
-							<p>The course will go through basics before introducing five components</p>
-							<ol>
-								<li>
-									<h4> Power BI Desktop</h4>
-									<p>You will learn how to build visuals (from previously modeled datasets) and collections of visuals to share as reports. You will need to install Power Bi desktop on your local computer so as to be able to transform and visualize your data.</p>
-                                	</li>
-								<li>
-									<h4>Power BI Service</h4>
-									<p>You will learn how to create, share, and consume business insights in a way that adds value to business processes most effectively. You will be able to use the Power BI service to share reports done in Power Bi Desktop with others.</p>
-								</li>
-								<li>
-									<h4>Power Query</h4>
-									<p>You will learn how to extract, transform (with Power Query Editor), and load (ETL) data from a Power BI graphical interface. At the end of this session, delegates will be able to manage work with various data sizes, data velocity (rate of change), across a breadth of data sources and data shapes.</p>
-								</li>
-
-                                <li>
-									<h4> Data Modelling</h4>
-									<p>
-                                        You learn how to logically combine multiple/different sources of data for effective extraction in Power BI. This session will enable candidates to deliver intuitive, accurate, and optimal models for data extraction.</p>
-                                	</li>
-								<li>
-									<h4>DAX</h4>
-									<p>
-                                        You’ll learn Data Analysis Expressions (DAX). DAX is a library of functions and operators from which formulas and expressions in Power BI, Analysis Services, and Power Pivot in Excel data models are built.
-                                        </p>
-								</li>
-								<li>
-									<h4>Excel Training</h4>
-									<!-- <p>You will learn how to extract, transform (with Power Query Editor), and load (ETL) data from a Power BI graphical interface. At the end of this session, delegates will be able to manage work with various data sizes, data velocity (rate of change), across a breadth of data sources and data shapes.</p> -->
-								</li>
-                                <li>
-									<h4>QHSE Training</h4>
-									<p>
-
-                                        GoldServe provides QHSE training to both corporate entities and individuals. Our training is currently delivered in class-room-style setting at client premises. We will soon provide online training courses. Our courses cover different levels in an organisation spanning from strategic to operational staff: QHSE for Directors, First-Aid at work, Managing safely, ToT, Hazard awareness, Accountability for safety, Risk Assessment, Permit to Work, PPE, Asset Integrity, COSHH, Working at Height, Manual Handling, Mechanical Lifting, HAZMAT, Health and Hygiene</p>
-								</li>
-							</ol>
-						</div> <!-- /.middle-text -->
-
-						<div class="bottom-text list-box-text wow fadeInUp">
-							<h3>WHY POWER BI</h3>
-							<p>Our Reasons for offering PowerBI</p>
-							<div class="row">
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4>Business Edge through Data</h4>
-											<p>From customer and employee data, metrics for company goals, to sales and acquisitions, businesses are drowning in data, but this data is only as good as your ability to interpret and communicate its meaning. That’s where Power BI (Business Intelligence) comes into play by showcasing the areas for focus and business advantage.</p>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4>Affordable Business Intelligence</h4>
-											<p>Because Power BI Desktop is free to use and Power BI Pro is available for a low monthly price per user, you can cost-effectively offer everyone BI and analytics capabilities to drive a data culture across your org.</p>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4>Turn Insights into Action</h4>
-											<p>Go from data to insights and insights to action with the Microsoft Power BI  combining Power BI with Power Apps and Power Automate to easily build business applications and automate workflows.</p>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4> Easy Access</h4>
-											<p>Connect to your data, wherever it is. Prep and model your data with ease.</p>
-										</li>
-									</ul>
-								</div>
-							</div>
-						</div> <!-- /.bottom-text -->
-					</div> <!-- /.box-wrapper -->
-
-                    <div class="box-wrapper">
-                        <h3>COURSE OBJECTIVES </h3>
-                        <p>Master Power BI Desktop data model, its components and most effective schemas Gain an understanding of the Power Query M language Master the basic concepts of Data Modelling and consequences of data model design decisions Learn how to import data from a variety of sources (including XLS and CSV files) Gain familiarity with standard DAX patterns and calculate Understand how to create professional reports, dashboards and share with users on Power BI service.</p>
-                        <div class="col-md-6" style="line-height: 2.5;">
-                            <ul>
-                                <li>
-                                    <h4>Direct benefits to you after the Course</h4>
-                                    <p>At the end of the course, you will be able to build dashboards based on different data sets. You will be ready to power your career with new-age knowledge in analytics that are on demand across many sectors globally. Be valuable and help businesses unlock hidden value in real-time using Analytics. Open up new career options in freelance Consultancy. Power BI freelancers make in the region of $75 an hour on free-lance work sites like upwork.com. Be able to tell better stories using data and agile process by creating Power BI data visualizations with proper report layout and structures. Understand the Power Bi Desktop data model, its components and most effective schemas.</p>
-                                </li>
-								<li>
-                                    <h4>Course Eligibility</h4>
-                                    <p>The course is applicable for all people that want to understand how to use the Power BI tools to create customized visual reports. Its also suitable for BI and reporting professionals, data analysts, and various professionals (in any sector) working with data.
-
-									</p>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div> <!-- /.box-wrapper -->
+                            </p> <br>
+							<!-- <p>One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in the future. The same is true as we experience the emotional sensation of stress from our first instances of social rejection ridicule. We quickly learn to fear and thus automatically avoid potentially stressful situations of all kinds, including the most common of all: making mistakes.</p>
+							<h2>A brand for a company is like a reputation for a person. You earn reputation by trying to do hard things well.</h2>
+							<h5>This response is important for our ability to learn from mistakes, but it alsogives rise to self-criticism, because it is part of the threat-protection system. </h5>
+							<p>One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in the future. The same is true as we experience the emotional sensation of stress from our first instances of social rejection ridicule. We quickly learn to fear and thus automatically.</p> -->
+                            <ul class="float-left">
+								
+								<li>NDT/MPI Inspections</li>
+								<li>Lifting Equipment Inspection</li>
+								<li>Project Management</li>
+							</ul>
+                        
+                        
+                        </div> <!-- /.blog-main-post -->
+						<!-- <div class="tag-option clearfix">
+							<ul class="float-left">
+								<li>Tag:</li>
+								<li><a href="#" class="tran3s">Travel,</a></li>
+								<li><a href="#" class="tran3s">Advanture,</a></li>
+								<li><a href="#" class="tran3s">Landscape</a></li>
+							</ul>
+							<ul class="float-right">
+								<li>Share:</li>
+								<li><a href="#" class="tran3s"><i class="fa fa-facebook" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-dribbble" aria-hidden="true"></i></a></li>
+								<li><a href="#" class="tran3s"><i class="fa fa-google-plus" aria-hidden="true"></i></a></li>
+							</ul>
+						</div>  -->
+					</div> <!-- /.wrapper -->
 				</div> <!-- /.container -->
-			</div> <!-- /.service-details -->
 
+				<!-- <div class="comment-area">
+					<div class="container">
+						<div class="wrapper">
+							<div class="comment-section">
+								<h2>2 comments</h2>
+								<div class="single-comment clearfix">
+									<img src="images/blog/20.jpg" alt="" class="float-left">
+									<div class="comment float-left">
+										<h6>Jannatul Fa.</h6>
+										<span>15 April, 2016 At 8:30am</span>
+										<p>One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in future. The same true we experience the emotional sensation of stress from our first.</p>
+										<button class="tran3s">Reply</button>
+									</div>
+								</div> 
+								<div class="single-comment clearfix no-border">
+									<img src="images/blog/21.jpg" alt="" class="float-left">
+									<div class="comment float-left">
+										<h6>Jannatul Fa.</h6>
+										<span>15 April, 2016 At 8:30am</span>
+										<p>One touch of a red-hot stove is usually all we need to avoid that kind of discomfort in future</p>
+										<button class="tran3s">Reply</button>
+									</div>
+								</div> 
+							</div>
+
+							<div class="leave-comment">
+								<h2>Leave A Comment</h2>
+								<p>sing in to post your comment or singup if you dont have any account.</p>
+								<form action="#">
+									<div class="row">
+										<div class="col-sm-6">
+											<input type="text" placeholder="Name *">
+										</div>
+										<div class="col-sm-6">
+											<input type="email" placeholder="Email *">
+										</div>
+										<div class="col-xs-12">
+											<textarea placeholder="Your Comment"></textarea>
+										</div>
+									</div>
+									<button class="p-bg-color hvr-trim-two">Post COmment</button>
+								</form>
+							</div> 
+						</div> 
+					</div> 
+				</div>  -->
+			</div> 
+			
+			
 
 
 @endsection
