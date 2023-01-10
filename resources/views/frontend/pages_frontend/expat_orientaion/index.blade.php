be6d855ed392036a69dbc2ccfceba89588503cc8
wandieinnocents <wandieinnocent2@gmail.com>
2023-01-10 14:30:57 +0300

    Expert orientation page

--- resources/views/frontend/pages_frontend/expat_orientaion/index.blade.php
+++ resources/views/frontend/pages_frontend/expat_orientaion/index.blade.php
@@ -13,148 +13,92 @@ Trainings
 			-->
 			<div class="inner-page-banner">
 				<div class="opacity">
-					<h1>Trainings Services</h1>
+					<h1>Expert Orientation</h1>
 					<ul>
 						<li><a href="/">Home</a></li>
 						<li>/</li>
-						<li>Trainings</li>
+						<li>Expert Orientation</li>
 					</ul>
 				</div> <!-- /.opacity -->
 			</div> <!-- /inner-page-banner -->
 
 
 
-			<!--
+			<!-- 
 			=============================================
-				Our Service Details
-			==============================================
+				Our Service V2
+			============================================== 
 			-->
-			<div class="service-details">
+			<div class="service-version-two">
 				<div class="container">
-					<div class="box-wrapper">
-						<img src="assets/frontend/images/home/bi2.png" alt="" class="feature-image">
-						<div class="title clearfix">
-							<h3 class="float-left">ANALYTICS COURSE</h3>
-							<a href="#" class="loan float-right">Apply Now</a>
-						</div> <!-- /.title -->
-						<div class="top-text">
-							<h4>This course is to impart hands on knowledge based on Power BI, a Business Intelligence and Data Analysis tool by Microsoft. The course will go through basics before introducing five components of Power BI (Power Pivot, Power Query, Power View, Power Map, and Power Q&A). At the end of the course you will be able to build dashboards based on different data sets and be ready to power your career with new edge knowledge in analytics that’s on demand across many sectors Analytics.</h4>
-                            <div class="row">
-								<div class="col-md-6 wow fadeInLeft"><p>
-                                    <ul>
+					<div class="single-service">
+						<div class="row">
+							<div class="col-md-6 col-xs-12">
+								<div class="image-box">
+									<img src="assets/frontend/images/expert.png" alt="">
+									<div class="opacity tran3s">
+										<p class="one">
 
-                                        <li>
-                                            <h5>POWER BI ANALYTICS</h5>
-                                            <p>Power BI analytics course is to impart hands-on knowledge based on Power BI, a Business Intelligence and Data Analysis tool by Microsoft.</p></div>
-                                        </p>
-                                        </li>
-                                    </ul>
-                                    <!-- <div class="col-md-6 wow fadeInRight"><p>Sometimes this is airony promoting  businesse product and services, because for fact that you want make ur business recognizable & earn sales of course you need with lot of effort.</p></div> -->
+                                            Getting an expatriate worker on board always involves an investment in training, physical relocation of the worker and of course the cost of the worker. They are always expected to start work immediately and be productive. This is where the orientation comes in. It is the process of making the expatriate worker feel at home, prepared for the new location of work, and of course ready to get started on the job.</p>
+										<!-- <p class="two">Gullu went to the woods. He took a fun ride and never came back.</p> -->
+									</div>
+								</div>
+							</div> <!-- /.col-lg-6 -->
+							<div class="col-md-6 col-xs-12 text">
+								<p>WHY EXPAT SERVICES</p>
+								<h5>Expatriate workers are often faced with a lot of challenges. They have to relocate, meet new people learn new cultures, new ways of doing things, deal with local bureaucracies etc. This can be stressful and our expat orientation program helps ease this transition.
+								
+								<a href="#" class="tran3s"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
+								<p>Some of our expat Services
+									<ol>
+										<li>Local Expat Orientation</li>
+										<li>Accomodation Searches and Occupier services</li>
+										<li>Airport Meet and Greet</li> 
+										<li>Local service onboarding</li> 
+										<li>School Search</li>
+										<li>Work Permit Support</li>
+										<li>School Location for children</li>
+										<li>e.t.c</li>
+									</ol>
+								</p>
+							</h5>
 							</div>
-						</div> <!-- /.top-text -->
+						</div> <!-- /.row -->
+					</div> <!-- /.single-service -->
 
-						<div class="middle-text list-box-text wow fadeInUp">
-							<h3>FIVE COMPONENTS FOR POWER BI TRAINING COURSE</h3>
-							<p>The course will go through basics before introducing five components</p>
-							<ol>
-								<li>
-									<h4> Power BI Desktop</h4>
-									<p>You will learn how to build visuals (from previously modeled datasets) and collections of visuals to share as reports. You will need to install Power Bi desktop on your local computer so as to be able to transform and visualize your data.</p>
-                                	</li>
-								<li>
-									<h4>Power BI Service</h4>
-									<p>You will learn how to create, share, and consume business insights in a way that adds value to business processes most effectively. You will be able to use the Power BI service to share reports done in Power Bi Desktop with others.</p>
-								</li>
-								<li>
-									<h4>Power Query</h4>
-									<p>You will learn how to extract, transform (with Power Query Editor), and load (ETL) data from a Power BI graphical interface. At the end of this session, delegates will be able to manage work with various data sizes, data velocity (rate of change), across a breadth of data sources and data shapes.</p>
-								</li>
+					<div class="single-service">
+						<div class="row">
+							<div class="col-md-6 col-xs-12 float-right">
+								<div class="image-box">
+									<img src="images/inner-page/2.png" alt="">
+									<div class="opacity tran3s">
+                                        <p>Local Awareness</p>
+										<p class="one">
 
-                                <li>
-									<h4> Data Modelling</h4>
-									<p>
-                                        You learn how to logically combine multiple/different sources of data for effective extraction in Power BI. This session will enable candidates to deliver intuitive, accurate, and optimal models for data extraction.</p>
-                                	</li>
-								<li>
-									<h4>DAX</h4>
-									<p>
-                                        You’ll learn Data Analysis Expressions (DAX). DAX is a library of functions and operators from which formulas and expressions in Power BI, Analysis Services, and Power Pivot in Excel data models are built.
-                                        </p>
-								</li>
-								<li>
-									<h4>Excel Training</h4>
-									<!-- <p>You will learn how to extract, transform (with Power Query Editor), and load (ETL) data from a Power BI graphical interface. At the end of this session, delegates will be able to manage work with various data sizes, data velocity (rate of change), across a breadth of data sources and data shapes.</p> -->
-								</li>
-                                <li>
-									<h4>QHSE Training</h4>
-									<p>
+                                            Your recently arrived team members need to know their surroundings, the more they do, the higher the likely hood of their increasing their productivity. </p>
+										<p class="two">Your HR Team may need local support
 
-                                        GoldServe provides QHSE training to both corporate entities and individuals. Our training is currently delivered in class-room-style setting at client premises. We will soon provide online training courses. Our courses cover different levels in an organisation spanning from strategic to operational staff: QHSE for Directors, First-Aid at work, Managing safely, ToT, Hazard awareness, Accountability for safety, Risk Assessment, Permit to Work, PPE, Asset Integrity, COSHH, Working at Height, Manual Handling, Mechanical Lifting, HAZMAT, Health and Hygiene</p>
-								</li>
-							</ol>
-						</div> <!-- /.middle-text -->
+                                            Many of the issues that occur on the ground might need immediate locally relevant advice. Your HR team(s) might not be able to advise on where to get a nanny, or a visit to the best schools, places of worship etc.… .</p>
+									</div>
+								</div>
+							</div> <!-- /.col-lg-6 -->
+							<div class="col-md-6 col-xs-12 text">
+								<!-- <p>Even the minor needs to get done</p> -->
+								<h5>
 
-						<div class="bottom-text list-box-text wow fadeInUp">
-							<h3>WHY POWER BI</h3>
-							<p>Our Reasons for offering PowerBI</p>
-							<div class="row">
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4>Business Edge through Data</h4>
-											<p>From customer and employee data, metrics for company goals, to sales and acquisitions, businesses are drowning in data, but this data is only as good as your ability to interpret and communicate its meaning. That’s where Power BI (Business Intelligence) comes into play by showcasing the areas for focus and business advantage.</p>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4>Affordable Business Intelligence</h4>
-											<p>Because Power BI Desktop is free to use and Power BI Pro is available for a low monthly price per user, you can cost-effectively offer everyone BI and analytics capabilities to drive a data culture across your org.</p>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4>Turn Insights into Action</h4>
-											<p>Go from data to insights and insights to action with the Microsoft Power BI  combining Power BI with Power Apps and Power Automate to easily build business applications and automate workflows.</p>
-										</li>
-									</ul>
-								</div>
-								<div class="col-md-6">
-									<ul>
-										<li>
-											<h4> Easy Access</h4>
-											<p>Connect to your data, wherever it is. Prep and model your data with ease.</p>
-										</li>
-									</ul>
-								</div>
+                                    <p>Even the minor needs to get done </p> So your team member recently arrived? They will need to get some local administration work and registrations done. What seems normal for locals might not be straight forward for your recently arrived expat team member. 
+									<p>Your HR Team may need local support  </p> Many of the issues that occur on the ground might need immediate locally relevant advice. Your HR team(s) might not be able to advise on where to get a nanny, or a visit to the best schools, places of worship etc..
+									<br>
+									Setting up office spaces  or moving from one workspace to another can get really intense and stressful at some point when one finds it hard to find a space of preference that falls in one’s budget   </h5>
+							
+									<a href="#" class="tran3s"><i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
 							</div>
-						</div> <!-- /.bottom-text -->
-					</div> <!-- /.box-wrapper -->
+						</div> <!-- /.row -->
+					</div> <!-- /.single-service -->
 
-                    <div class="box-wrapper">
-                        <h3>COURSE OBJECTIVES </h3>
-                        <p>Master Power BI Desktop data model, its components and most effective schemas Gain an understanding of the Power Query M language Master the basic concepts of Data Modelling and consequences of data model design decisions Learn how to import data from a variety of sources (including XLS and CSV files) Gain familiarity with standard DAX patterns and calculate Understand how to create professional reports, dashboards and share with users on Power BI service.</p>
-                        <div class="col-md-6" style="line-height: 2.5;">
-                            <ul>
-                                <li>
-                                    <h4>Direct benefits to you after the Course</h4>
-                                    <p>At the end of the course, you will be able to build dashboards based on different data sets. You will be ready to power your career with new-age knowledge in analytics that are on demand across many sectors globally. Be valuable and help businesses unlock hidden value in real-time using Analytics. Open up new career options in freelance Consultancy. Power BI freelancers make in the region of $75 an hour on free-lance work sites like upwork.com. Be able to tell better stories using data and agile process by creating Power BI data visualizations with proper report layout and structures. Understand the Power Bi Desktop data model, its components and most effective schemas.</p>
-                                </li>
-								<li>
-                                    <h4>Course Eligibility</h4>
-                                    <p>The course is applicable for all people that want to understand how to use the Power BI tools to create customized visual reports. Its also suitable for BI and reporting professionals, data analysts, and various professionals (in any sector) working with data.
-
-									</p>
-                                </li>
-
-                            </ul>
-                        </div>
-                    </div> <!-- /.box-wrapper -->
 				</div> <!-- /.container -->
-			</div> <!-- /.service-details -->
+			</div> <!-- /.service-version-two -->
+			
 
 
 
